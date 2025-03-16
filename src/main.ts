4515f425ea11f1535850c5d52921f03707c95463
Emmanuel Fosu <emmanuel.fosuduffour@gmail.com>
2025-03-16 19:41:31 +0000

    Feat: Styles and Animation is applied

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,67 @@
 import './style.css'
+import gsap from 'gsap';
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  const body = document.body;
+  const mobileNavContainer = document.querySelector(".mobile-header-container") as HTMLDivElement;
+  const menuButton = document.querySelector(".menu-button") as HTMLButtonElement;
+
+  menuButton.addEventListener("click", () => {
+    if (body.classList.contains("menu-active")) {
+      body.classList.remove("menu-active");
+      body.classList.add("menu-inactive");
+      onMenuInactive();
+
+      return;
+    }
+
+    if (body.classList.contains("menu-inactive")) {
+      body.classList.remove("menu-inactive");
+      body.classList.add("menu-active");
+      onMenuActive();
+
+      return;
+    }
+  
+  })
+
+  window.addEventListener("resize", () => {
+    if (Math.floor(window.innerWidth) >= 750) {
+      if (body.classList.contains("menu-active")) {
+        body.classList.remove("menu-active");
+        body.classList.add("menu-inactive");
+
+        gsap.to(mobileNavContainer, {
+          right: "-100%"
+        });
+        return;
+      }
+    }
+  })
+
+  function onMenuActive(): void {
+    gsap.to(mobileNavContainer, {
+      right: 0,
+      onStart: () => {
+        menuButton.disabled = true;
+      },
+      onComplete: () => {
+        menuButton.disabled = false;
+      }
+    })
+  }
+
+  function onMenuInactive(): void {
+    gsap.to(mobileNavContainer, {
+      right: "-100%",
+      onStart: () => {
+        menuButton.disabled = true;
+      },
+      onComplete: () => {
+        menuButton.disabled = false;
+      }
+    })
+  }
+
+})
